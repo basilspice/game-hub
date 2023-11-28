8d68110975956c19c1f0aa7e0eb7c9e84f9a3e98
basilspice <basil.breton@gmail.com>
2023-11-28 11:09:59 -0800

    added navbar foundation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-
-
+import NavBar from "./components/navBar";
 
 function App() {
-  return <Grid templateAreas={{
-    base: `"nav" "main"`,
-    lg: '"nav nav", "aside main"'}}>
-
-    <GridItem area='nav' bg='coral'>Nav</GridItem>
-    <Show above="lg">
-    <GridItem area='aside' bg='gold'>Aside</GridItem>
-
-    </Show>
-    <GridItem area='main ' bg='dodgerblue'>Main</GridItem>
-
-  </Grid>
-
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: '"nav nav" "aside main"',
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main " bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 export default App;
