8606d920676636b846bf5c3def2a9da47dbe3325
basilspice <basil.breton@gmail.com>
2023-11-29 15:57:43 -0800

    removed ugly button,that took over everything,  refactored button to look better

--- src/components/StickyFooter.tsx
+++ src/components/StickyFooter.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@chakra-ui/react"
 
 const StickyFooter = () => {
     return(
     <div className="footer">
     <footer>
-     <a href="https://github.com/basilspice/game-hub" target="_blank"><button>Source Code</button></a>
+     <a href="https://github.com/basilspice/game-hub" target="_blank"><button className='buttonSticky'>Source Code</button></a>
     </footer>
     </div>
     )}
