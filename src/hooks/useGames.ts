07a034e8b2b1d3bd844fc8d3ed93170d1bcb7701
basilspice <basil.breton@gmail.com>
2023-11-29 12:23:26 -0800

    refacotored: extract query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
-
 
 export interface Platform {
   id: number;
@@ -16,7 +16,13 @@ export interface Game {
   metacritic: number;
 }
 
-
-const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', {params: {genres: selectedGenre?.id}}, [selectedGenre?.id]);
+const useGames = (
+  gameQuery: GameQuery
+) =>
+  useData<Game>(
+    "/games",
+    { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id } },
+    [gameQuery]
+  );
 
 export default useGames;
