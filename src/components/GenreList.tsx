2d5b96554f527fb45a4ea5a79f38203cd00facd1
basilspice <basil.breton@gmail.com>
2023-11-29 11:28:51 -0800

    highlighting and changing font of selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,9 +6,12 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
     onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre}: Props) => {
+
+
+const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />
@@ -23,7 +26,7 @@ const GenreList = ({onSelectGenre}: Props) => {
             borderRadius={8}
             src={getCroppedImageUrl(genre.image_background)}
           />
-          <Button onClick={() =>onSelectGenre(genre)}fontSize='lg' variant='link'>{genre.name}</Button>
+          <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} color={genre.id === selectedGenre?.id ? 'dodgerblue' : 'white'} onClick={() =>onSelectGenre(genre)}fontSize='lg' variant='link'>{genre.name}</Button>
         </HStack>
       </ListItem>
     ))}
